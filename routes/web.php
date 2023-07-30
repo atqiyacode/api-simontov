d318a5392a396ad7e49956b3d23a5731ba81f9ef
atqiyacode <atqiya@atqiyacode.com>
2023-07-31 02:08:20 +0700

    update web route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return [
+        'Framework Laravel Version' => app()->version(),
+        'Status' => app()->isProduction() ? 'Production' : 'Developement',
+        'Developer' => 'VEPRO APP DEVELOPER',
+    ];
 });
