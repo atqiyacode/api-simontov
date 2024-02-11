8ba10383fde1717a91f4edd7b8e8c20fd25f1ffc
atqiyacode <atqiya@atqiyacode.com>
2024-02-12 01:06:06 +0700

    update flowarate seeder

--- database/seeders/FlowrateSeeder.php
+++ database/seeders/FlowrateSeeder.php
@@ -3,8 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Flowrate;
-use App\Models\Location;
-use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class FlowrateSeeder extends Seeder
@@ -16,60 +14,7 @@ class FlowrateSeeder extends Seeder
      */
     public function run(): void
     {
-        $list = [];
-        // for ($i = 1; $i <= 1000; $i++) {
-        //     $randpattern = '';
-        //     while (strlen($randpattern) < fake()->numberBetween($min = 0, $max = 14))
-        //         $randpattern .= rand(0, 1);
-
-        //     $locationId = Location::pluck('id')->random();
-
-        //     $dummy = [
-        //         'created_at' => Carbon::now()->subDay()->addMinutes(-1 * $i)->format('Y-m-d H:i'),
-        //         'mag_date' => Carbon::now()->subDay()->addMinutes(-1 * $i)->format('Y-m-d H:i'),
-        //         // 'mag_date_time' => Carbon::now()->subDay()->addMinutes(-1 * $i)->timestamp,
-        //         'flowrate' => fake()->randomFloat($nbMaxDecimals = NULL, $min = 0, $max = 100),
-        //         'unit_flowrate' => 'm3/h',
-        //         'totalizer_1' => $i * 3,
-        //         // 'totalizer_1' => fake()->randomFloat($nbMaxDecimals = NULL, $min = 0, $max = 100),
-        //         'totalizer_2' => fake()->randomFloat($nbMaxDecimals = NULL, $min = 0, $max = 100),
-        //         'totalizer_3' => fake()->randomFloat($nbMaxDecimals = NULL, $min = 0, $max = 100),
-        //         'unit_totalizer' => 'm3',
-        //         'analog_1' => fake()->randomFloat($nbMaxDecimals = NULL, $min = 0, $max = 5),
-        //         'pressure' => fake()->randomFloat($nbMaxDecimals = NULL, $min = 0, $max = 5),
-        //         'status_battery' => fake()->numberBetween($min = 10, $max = 100),
-        //         'alarm' => fake()->numberBetween($min = 10, $max = 150),
-        //         'bin_alarm' => $randpattern,
-        //         // 'file_name' => 'FILE-' . fake()->numberBetween($min = 1, $max = 5),
-        //         'file_name' => 'MEDAN',
-
-        //         'location_id' => $locationId,
-
-        //         'ph' => fake()->randomFloat($nbMaxDecimals = NULL, $min = 6, $max = 8),
-        //         'cod' => fake()->randomFloat($nbMaxDecimals = NULL, $min = 0, $max = 10),
-        //         'cond' => fake()->randomFloat($nbMaxDecimals = NULL, $min = 0, $max = 100),
-        //         'level' => fake()->randomFloat($nbMaxDecimals = NULL, $min = 0, $max = 100),
-        //         'do' => fake()->randomFloat($nbMaxDecimals = NULL, $min = 0, $max = 100),
-
-        //         'do_alarm_hi' => fake()->boolean(),
-        //         'do_alarm_lo' => fake()->boolean(),
-        //         'pres_alarm_hi' => fake()->boolean(),
-        //         'pres_alarm_lo' => fake()->boolean(),
-        //         'ph_alarm_hi' => fake()->boolean(),
-        //         'ph_alarm_lo' => fake()->boolean(),
-
-        //         'fm_status' => $randpattern,
-        //         'fm_err_code' => $randpattern,
-
-        //         'pln_stat' => fake()->boolean(),
-        //         'panel_stat' => fake()->boolean(),
-
-        //         'location_id' => $locationId,
-        //     ];
-        //     $dummy['log_data'] = json_encode($dummy);
-        //     $list[] = $dummy;
-        // }
-        $files = glob(public_path('backup-flowrates/flowrates_*.json'));
+        $files = glob(public_path('flowrates-backup/flowrates_*.json'));
 
         foreach ($files as $file) {
             $data = json_decode(file_get_contents($file), true);
@@ -86,10 +31,5 @@ class FlowrateSeeder extends Seeder
                 $this->command->error("Invalid JSON file format: $file");
             }
         }
-
-        // $chunks = array_chunk($list, 500);
-        // foreach ($chunks as $chunk) {
-        //     Flowrate::insert($chunk);
-        // }
     }
 }
